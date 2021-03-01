262f73cf94d6c7c28ae0f4b01c9f044475c909ed
CodeWorld3 <lthompson1206@gmail.com>
2021-03-01 18:05:34 -0500

    edit post page

--- src/components/Post.js
+++ src/components/Post.js
@@ -25,7 +25,7 @@ export default function Post() {
 <main className="bg-gray-700 min-h-screen p-12"> 
     <section className="container mx-auto">
         <h1 className="text-5xl text-gray-100 flex justify-center cursive">Credentials</h1>
-        <h2 className="text-lg text-gray-100 flex justify-center mb-12">Welcome to my page of my accomplishments</h2>
+        <h2 className="text-lg text-gray-100 flex justify-center mb-12">Welcome to my page of my Resume & Certifications</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
             {postData && 
             postData.map((post, index) => (
