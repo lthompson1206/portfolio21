db064b63f76241297d10eae182ddb09f0dde830c
CodeWorld3 <lthompson1206@gmail.com>
2022-11-06 15:34:10 -0500

    added back sectional cards

--- scripts.js
+++ scripts.js
@@ -4,3 +4,22 @@
 /* font-family: 'Aguafina Script', cursive; */
 /* font-family: 'Rouge Script', cursive; */
 // font-family: 'Roboto', sans-serif;
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach((reveal) => {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveal.getBoundingClientRect().top;
+        var elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveal.classList.add("active");
+        } else {
+            reveal.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener("scroll", reveal);
